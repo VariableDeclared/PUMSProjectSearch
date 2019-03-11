a5b3f2be0a3c081e0fcf3e53b22192096761bfe5
Peter J De Sousa <petedes@live.com>
2019-03-11 10:06:40 +0000

    Start on MVC Services - Requires REST.
    Going back to pj/apsw in order to implement
    non RESTFul changes (Clicking on project names
    and insertion of project owners fix)

--- src/main/java/com/pete/pumsprojectsearch/session/ProjectDetailController.java
+++ src/main/java/com/pete/pumsprojectsearch/session/ProjectDetailController.java
@@ -7,10 +7,13 @@ package com.pete.pumsprojectsearch.session;
 
 import com.pete.pumsprojectsearch.persistence.entities.Project;
 import com.pete.pumsprojectsearch.persistence.facades.ProjectFacade;
-import java.util.List;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
-
+import javax.ws.rs.Path;
+import javax.mvc.annotation.Controller;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 
 /**
  *
@@ -18,17 +21,32 @@ import javax.enterprise.context.Dependent;
  */
 @Named(value = "projectDetailController")
 @Dependent
+@Controller
+@Path("Projects")
 public class ProjectDetailController
 {
 
-    List<Project> projects;
+    @EJB
     ProjectFacade projectEjb;
+    
+    Project selectedProject;
+    
+    @GET
+    @Path("/projects/{id}")
+    public String getProjectDetail(@PathParam("id") int projectId) {
+        this.selectedProject = projectEjb.find(projectId);
+        return "projects/detail.xhtml";
+    }
+    
+    public Project getProject() {
+        return this.selectedProject;
+    }
     /**
      * Creates a new instance of ProjectDetail
      */
     public ProjectDetailController()
     {
-
+        
     }
     
     public String prepareProjectsHome() {
